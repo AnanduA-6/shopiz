15ae1492d667cdb2dd9932cecadce3cf06e47b12
Anandu A <anandua606@gmail.com>
2023-12-10 23:33:13 +0530

    singleProductPage

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ document.querySelector('.closeBtn').addEventListener('click',()=>{
     navBarContainer.style.opacity="0"
     navBarContainer.classList.remove('navBarContainerOpen')
 })
+
+let itemCount=document.querySelector('.itemCount')
+
+document.querySelector('.increment').addEventListener('click',()=>{
+    changeCount('i')
+})
+document.querySelector('.decrement').addEventListener('click',()=>{
+    changeCount('d')
+})
+
+function changeCount(val){
+    let num=Number(itemCount.textContent)
+    if(val==='i'){
+        itemCount.textContent=num+1;
+    } 
+    if(val==="d"){
+        if(num!=0){
+            itemCount.textContent=num-1; 
+        }
+    }
+    
+}
